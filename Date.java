fdeeaf9f6806670a45ede85698594765c2cdb842
edzaans-git <g00377012@gmit.ie>
2020-02-20 14:47:50 +0000

    This classroom

--- Date.java
+++ Date.java
@@ -4,28 +4,57 @@
 public class Date {
     
     // Instance Variables
-    private int day;
-    private int month;
-    private int year;
+    int day;
+    int month;
+    int year;
+
+    String event;
 
     // Default Constructor (No Parameters)
     public Date(){
         //Your code here
+        //default values = 0, can be changed if needed
+        day = 1;
+        month = 1;
+        year = 1;
+        event = "Nothing on today";
+        
     }
 
     // Parameterized constructor (3 Parameters)
-    public Date(int newDay, int newMonth, int newYear){
-        // Your code here.
+    public Date(int day, int month, int year){
+        
+     this(day, month);
+      this.year = year;
     }
     
     // Parameterized constructor (2 Parameters)
+    public Date(int day, int month){
+        // Your code here.
+      this(day);
+      this.month = month;
+    }
     // Your code here.
     
     // Parameterized constructor (1 Parameter)
+       public Date(int day){
+        // Your code here.
+       this.day = day;
+       //year = 2000;
     // Your code here.
-    
+       }
+       
+           // Parameterized constructor (3 Parameters)
+    public Date(int day, int month, int year, String event){
+        this(day, month, year);
+        this.event = event;
+     
+    }
+
     // A simple print method
     public void printDate(){
+
+        System.out.println(this.day+ "/" +this.month+ "/" +this.year);
         // Print the date like this: day/month/year
         // Your code here
     }
--- Main.java
+++ Main.java
@@ -4,7 +4,25 @@
 public class Main {
     public static void main(String[] args) {
         // Create a Date object using the default constructor
+        Date dateObject1 = new Date();
+        System.out.println(dateObject1.year);
         // Create 3 more Date objects using all the paramiterised constructors
+        Date dateObject2 = new Date(20, 02, 2020);
         // Call the printDate() method for each object you create.
+        System.out.println("Today is Thursday :" +dateObject2.year+"/"+dateObject2.month+"/"+dateObject2.day);
+
+
+        Date dateObject3 = new Date(31);
+        System.out.println(dateObject3.day);
+        System.out.println(dateObject3.year);
+
+        Date dateObject4 = new Date(15, 12);
+        System.out.println(dateObject4.month);
+
+        Date dateObject5 = new Date(17, 03, 2020, "St Patyricks Day");
+        System.out.println(dateObject5.event);
+
+        
+
     }
 }
